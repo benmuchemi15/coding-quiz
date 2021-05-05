55fdfeffed5eb61381918b547ae2557edd8b8f83
Benson Muchemi <Ben@Bensons-MacBook-Pro.local>
2021-05-05 18:55:04 -0400

    assets folder

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+const startButton = document.getElementById("ques1")
+
+startButton.addEventListener("click", startQuiz)
+
+function startQuiz() {
+
+}
+
+
+var quiz = [
+    questions: [
+        q: "Commonly used data types DO NOT Include:",
+        options: ["strings", "boolean", "alerts", "numbers"]
+        answer: 1
+    ]
+]
+
+function startQuestion() {
+
+}
+
