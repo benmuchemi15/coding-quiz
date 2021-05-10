08dd0eff2a35e56827ae0814f4c5b2c94410e5b3
Benson Muchemi <Ben@Bensons-MacBook-Pro.local>
2021-05-10 00:03:12 -0400

    updated js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,133 @@
-function startQuiz(){
-    // variable to store output
-    const quiz = [];
+// var generateBtn = document.querySelector("start");
 
-    //questions
-    theQuestions.array.forEach((currentQuestion, questionNumber) => {
+// function startQuiz(){
+//     // variable to store output
+//     document.getElementsByClassName("container");
+//     while(parent.firstChild){
+
+//     }
+
+//     const quiz = [];
+
+//     //questions
+//     theQuestions.forEach((currentQuestion, questionNumber) => {
         
-        // variable for possible answers
-        const answer = [];
+//         // variable for possible answers
+//         const answer = [];
 
-        for(letter in currentQuestion.answer){
-            answer.push(
-                `<label>
-                <input type="radio" name="question${questionNumber}" value="${letter}">
-                ${letter} :
-                ${currentQuestion.answers[letter]}
-              </label>`
-            );
-        }
+//         for(letter in currentQuestion.answer){
+//             answer.push(
+//                 `<label>
+//                 <input type="radio" name="question${questionNumber}" value="${letter}">
+//                 ${letter} :
+//                 ${currentQuestion.answer[letter]}
+//               </label>`
+//             );
+//         }
 
-        answer.push(
-            `<div class="slide">
-              <div class="question"> ${currentQuestion.question} </div>
-              <div class="answers"> ${answers.join("")} </div>
-            </div>`
-        )
-    });
+//         answer.push(
+//             `<div class="slide">
+//               <div class="question"> ${currentQuestion.question} </div>
+//               <div class="answers"> ${answer.join("")} </div>
+//             </div>`
+//         )
+//     });
 
-    contQuiz.innerHtml = output.join('');
+//     contQuiz.innerHtml = output.join('');
+// }
+// // answers from the quiz
+// function theResults(){
+//     let correctNum = 0
+
+//     // for each question...
+//     theQuestions.forEach( (currentQuestion, questionNumber) => {
+//         // find selected answer
+//         const ansContainer = ansContainers[questionNumber];
+//         const userAnswer = (ansContainer.querySelector(selector) || {}).value;
+  
+//         // if answer is correct
+//         if(userAnswer === currentQuestion.correctAnswer){
+//           // add to the number of correct answers
+//           numCorrect++;
+//         }
+//     });
+  
+      
+// }
+
+// //
+// const theQuestions = [
+//     {
+//       question: "Commonly Used data types do not include",
+//       answers: {
+//         a: "strings",
+//         b: "booleans",
+//         c: "alerts",
+//         d: "numbers"
+//       },
+//       correctAnswer: "c"
+//     },
+//     {
+//       question: "The condition in an if/else statement is enclosed with?",
+//       answers: {
+//         a: "quotes",
+//         b: "curly brackets",
+//         c: "parentheses",
+//         d: "square brackets"
+//       },
+//       correctAnswer: "c"
+//     },
+//     {
+//       question: "String Values must be enclosed within____ when being assigned to variables",
+//       answers: {
+//         a: "commas",
+//         b: "curly brackets",
+//         c: "quotes",
+//         d: "parenthes"
+//       },
+//       correctAnswer: "d"
+//     },
+//     {
+//         question: "A ver uselful tool used during development and debugging for printing content to debugger is:",
+//         answers: {
+//           a: "JavaScript",
+//           b: "terminl",
+//           c: "loops",
+//           d: "console.log"
+//         },
+//         correctAnswer: "d"
+//       },
+//       {
+       
+//       }
+    
+//   ];
+
+//   generateBtn.addEventListener("click", startQuiz);
+var startBtn = document.getElementById("start");
+console.log(startBtn);
+
+if (startBtn.addEventListener) {
+  startBtn.addEventListener("click", startQuiz, false);
+} else if (startBtn.attachEvent) {
+  startBtn.attachEvent('onclick', startQuiz);
 }
+
+function startQuiz(){
+    let container = document.getElementById("test1");
+    console.log(container);
+    while (container.firstChild) {
+      container.removeChild(container.lastChild);
+    }
+
+    theQuestions.forEach((questionElement) => {
+      let node = document.createElement("P");
+      let textNode = document.createTextNode(questionElement.question);
+      node.appendChild(textNode);
+      container.appendChild(node);
+    });
+}
+
 // answers from the quiz
 function theResults(){
     let correctNum = 0
@@ -49,7 +149,7 @@ function theResults(){
 }
 
 //
-const myQuestions = [
+const theQuestions = [
     {
       question: "Commonly Used data types do not include",
       answers: {
@@ -81,7 +181,7 @@ const myQuestions = [
       correctAnswer: "d"
     },
     {
-        question: "A ver uselful tool used during development and debugging for printing content to debugger is:",
+        question: "A very uselful tool used during development and debugging for printing content to debugger is:",
         answers: {
           a: "JavaScript",
           b: "terminl",
